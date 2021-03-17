e0812dc01ef8f81d21f8b08634aa3c13769641af
Allan823 <79044346+Allan823@users.noreply.github.com>
2021-03-17 20:19:14 +0500

    Add files via upload

---
+++ Pingvin zadanie B.py
@@ -0,0 +1,13 @@
+pic = (
+     "  _~_  ",
+     " (o o) ",
+     " / V \\ ",
+     "/( _ )\\",
+     " ^^ ^^ "
+)
+
+n = 3
+for i in pic:
+    solution = ' '.join([i] * n)
+    print(solution)
+
---
+++ Yabloko  zadanie C.py
@@ -0,0 +1,6 @@
+n = int(input('apple: '))
+s = n
+k = int(input('shkolnikov: '))
+s1 = k
+m = s % s1
+print(m)
