1daac84a3a41e9ab343f525d09305057e3d5d1fa
Allan823 <79044346+Allan823@users.noreply.github.com>
2021-04-26 18:43:10 +0500

    Add files via upload

--- E.py
+++ E.py
@@ -1,8 +1,9 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
-if x1 == x2 or y1 == y2:
-    print('True')
-else:
-    print('False')
+def solution(x1,y1,x2,y2):
+    a = x1-x2
+    b = y1-y2
+    if(a or b) >1 or (a or b) < -1:
+        return False
+    else:
+        return True
+
+print(solution(4,4,5,5))
