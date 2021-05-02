4810facb43b24c3b2486b3cf4af70f425d4b2ce2
Allan823 <79044346+Allan823@users.noreply.github.com>
2021-05-02 22:48:21 +0500

    Update D.py

--- D.py
+++ D.py
@@ -1,19 +1,15 @@
-def josephus(ls, skip):
-    skip -= 1 
-    idx = skip
-    while len(ls) > 1:
-        print(ls.pop(idx)) 
-        idx = (idx + skip) % len(ls)
-    print('survivor: ', ls[0])
+def solution(n,k):
+    k -= 1 
+    idx = k
+    while len(n) > 1:
+        print(n.pop(idx))
+        idx = (idx + k) % len(n)
+    return f"solution:{n[0]}"
+
+    #print('survivor:', n[0])
 
 
-output
 
-josephus([1,2,3,4,5,6,7], 3)
-3
-6
-2
-7
-5
-1
-survivor:  4
+
+#solution([1,2,3,4,5,6,7], 3)
+
